b628205eac290b44c77e1820b153e135028d24dd
Christopher Mühl <hello@muehl.dev>
2025-12-19 09:24:56 +0100

    Configure XDG portal with GTK backend for file dialogs

--- modules/nixos/wm/default.nix
+++ modules/nixos/wm/default.nix
@@ -25,6 +25,7 @@ in {
       wl-clipboard
       libsForQt5.qtstyleplugin-kvantum
       xwayland-satellite
+      nautilus # xdg-gnome needs this but we don't use gnome???
     ];
 
     programs.niri = {
@@ -52,7 +53,13 @@ in {
       };
     };
 
-    # xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
+    xdg.portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+      extraPortals = [pkgs.xdg-desktop-portal-gtk];
+      configPackages = [pkgs.xdg-desktop-portal-gtk];
+      config.common.default = "gtk";
+    };
 
     security.polkit.enable = true;
   };
